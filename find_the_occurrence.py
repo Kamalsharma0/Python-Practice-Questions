2422e33438ef51cbb8c5937659690dfa4f9e0807
Kamal Kumar <sharmakamal1605@gmail.com>
2025-08-08 22:49:37 +0530

    More Files added

---
+++ find_the_occurrence.py
@@ -0,0 +1,4 @@
+# WAP to find the occurrence of ‘$’ in a String.
+name = "1 INR = 20$"
+
+print(name.find("$"))  # show index of char
---
+++ fname_with_func.py
@@ -0,0 +1,8 @@
+#implement function
+
+def my_function(fname):
+  print(fname + " Kumar")
+
+my_function("Ram")
+my_function("Shyam")
+my_function("Kamal")
