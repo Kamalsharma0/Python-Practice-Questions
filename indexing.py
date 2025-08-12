b2f155b36a79911e38ec1150ec24d6c8b0f9e1c9
Kamal Kumar <sharmakamal1605@gmail.com>
2025-08-12 22:30:22 +0530

    Add files via upload

---
+++ indexing.py
@@ -0,0 +1,15 @@
+# A p n a _ C o l l e g e
+# 0 1 2 3 4 5 6 7 8 9 10 11
+
+mystr = "Kamal Sharma"
+idx0 = mystr[0]
+idx1 = mystr[1]
+idx2 = mystr[2]
+idx3 = mystr[3]
+idx4 = mystr[4]
+
+print(idx0)
+print(idx1)
+print(idx2)
+print(idx3)
+print(idx4)
